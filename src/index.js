68553cffff43ae2ed4c44d946fa37de9306bde0f
Brock Elgart <bwelgart@gmail.com>
2018-09-04 12:20:35 -0400

    basic style working
    need to fix fonts and expanding image

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import jqueryBackstretch from 'jquery-backstretch';
 import templatemo from './js/templatemo_main.js';
-import bootstrapCss from 'bootstrap-css-only';
+import bootstrapCss from 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/templatemo_main.css';
