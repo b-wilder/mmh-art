a2bb9362d01010e345037cb924ea00ac4b6b0374
Brock Elgart <bwelgart@gmail.com>
2018-09-11 11:45:01 -0400

    attempt to create image details page

--- src/js/templatemo_main.js
+++ src/js/templatemo_main.js
@@ -10,73 +10,138 @@ jQuery(function($) {
 
     $(document).ready( function() {
 
-        // backstretch for background image
-        var defaultImgSrc = $('img.main-img').attr('src');
-        $.backstretch(defaultImgSrc, {speed: 500});
+      // backstretch for background image
+      var defaultImgSrc = $('img.main-img').attr('src');
+      $.backstretch(defaultImgSrc, {speed: 500});
 
-	// for responsive-menu
-	$("#m-btn").click(function(){
-		$("#responsive").toggle();
-	});
-
-
-        // copy menu list to responsive menu
-        var mainMenuList = $('#menu-list').html();
-        $('#responsive').html(mainMenuList);
-
-	//for image slide on menu item click(normal) and responsive
-	$("#menu-list a, #responsive a").on('click',function(e){
+	    // for responsive-menu
+      $("#m-btn").click(function(){
+        $("#responsive").toggle();
+	    });
 
 
-			if(this.className == "external") {
+      // copy menu list to responsive menu
+      var mainMenuList = $('#menu-list').html();
+      $('#responsive').html(mainMenuList);
+
+	    //for image slide on menu item click(normal) and responsive
+	    $("#menu-list > li > a, #responsive a").on('click',function(e){
+
+
+  			if(this.className == "external") {
+          return;
+        }
+
+  			e.preventDefault();
+
+        if (menuDisabled == false) // check the menu has disabled?
+        {
+            menuDisabled = true; // disable to menu
+
+            var name = $(this).attr('href');
+            var imgDetails = $("#img-details");
+            $('#menu-list li').removeClass('active');
+            $('#responsive li').removeClass('active');
+
+
+            //  set active to both menu
+            var menuClass = $(this).parent('li').attr('class');
+            $('.'+menuClass).addClass('active');
+
+            // hide responsive menu
+            $("#responsive").hide();
+
+            // get image url and assign to backstretch for background
+            /*
+            var imgSrc = $("img"+name+"-img").attr('src');
+            $.backstretch(imgSrc, {speed: 500}); //backstretch for background fade in/out
+            */
+
+            // content slide in/out
+            $("section.active").animate({left:$("section.active").outerWidth()}, 400,function(){
+              imgDetails.removeClass("active");
+              imgDetails.hide();
+              $(this).removeClass("active");
+              $(this).hide();
+              $(name+"-text").show();
+              $(name+"-text").animate({left:'0px'},400,function(){
+                $(this).addClass("active");
+
+                $.backstretch("resize"); // resize the background image
+
+                menuDisabled = false; // enable the menu
+              });
+            });
+
+            $(this).find(".lazy-load").each(function (index, element) {
+              var img = new Image();
+              img.src = $(element).data("lazy-load-image");
+              if (typeof $(element).data("image-classname" !== "undefined"))
+                img.className = $(element).data("image-classname");
+              $(element).append(img);
+            });
+        }
         return;
-      }
+      });
 
-			e.preventDefault();
+      $(".img-column > a").on('click',function(e){
 
-            if (menuDisabled == false) // check the menu has disabled?
-            {
-                menuDisabled = true; // disable to menu
+  			e.preventDefault();
 
-                var name = $(this).attr('href');
-                $('#menu-list li').removeClass('active');
-                $('#responsive li').removeClass('active');
+        if (menuDisabled == false) // check the menu has disabled?
+        {
+            menuDisabled = true; // disable to menu
 
-                //  set active to both menu
-                var menuClass = $(this).parent('li').attr('class');
-                $('.'+menuClass).addClass('active');
+            var imgSrc = $(this).find("img.art-img").attr("href");
+            var imgDetails = $("#img-details");
+            var imgElement = $("#img-details img");
+            var activeSection = $("section.active");
 
-                // hide responsive menu
-                $("#responsive").hide();
+            $('#menu-list li').removeClass('active');
+            $('#responsive li').removeClass('active');
 
-                // get image url and assign to backstretch for background
-                var imgSrc = $("img"+name+"-img").attr('src');
-                $.backstretch(imgSrc, {speed: 500}); //backstretch for background fade in/out
 
-                // content slide in/out
-                $("section.active").animate({left:$("section.active").outerWidth()}, 400,function(){
-                    $(this).removeClass("active");
-                    $(this).hide();
-                    $(name+"-text").show();
-                    $(name+"-text").animate({left:'0px'},400,function(){
-                        $(this).addClass("active");
+            imgDetails.addClass("active");
+            //  set active to both menu
+            /*
+            var menuClass = $(this).parent('li').attr('class');
+            $('.'+menuClass).addClass('active');
+            */
 
-                        $.backstretch("resize"); // resize the background image
+            // hide responsive menu
+            //$("#responsive").hide();
 
-                        menuDisabled = false; // enable the menu
-                    });
-                });
+            // get image url and assign to backstretch for background
+            /*
+            var imgSrc = $("img"+name+"-img").attr('src');
+            $.backstretch(imgSrc, {speed: 500}); //backstretch for background fade in/out
+            */
 
-                $(".lazy-load").each(function (index, element) {
-                    var img = new Image();
-                    img.src = $(element).data("lazy-load-image");
-                    if (typeof $(element).data("image-classname" !== "undefined"))
-                        img.className = $(element).data("image-classname");
-                    $(element).append(img);
-                });
-            }
-            return;
-	});
+            // content slide in/out
+            imgElement.attr("href",imgSrc);
+            imgDetails.animate({left:imgDetails.outerWidth()}, 400,function(){
+              activeSection.removeClass("active");
+              activeSection.hide();
+              imgDetails.show();
+              imgDetails.animate({left:'0px'},400,function(){
+                imgDetails.addClass("active");
+
+                $.backstretch("resize"); // resize the background image
+
+                menuDisabled = false; // enable the menu
+              });
+            });
+
+            imgDetails.find(".lazy-load").each(function (index, element) {
+              var img = new Image();
+              img.src = $(element).data("lazy-load-image");
+              if (typeof $(element).data("image-classname" !== "undefined"))
+                img.className = $(element).data("image-classname");
+              $(element).append(img);
+            });
+        }
+        return;
+      });
 
     });
 
